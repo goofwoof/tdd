012eee68680527e315c95c7671a7302b0ffae3b1
goofwoof <heli0086@outlook.com>
2020-08-03 23:06:06 +0800

    补充测试用例

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,16 +1,39 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MommifierTest {
     @Test
-    void should_insert_mommy_for_each_continuous_set_of_vowels_when_insert_given_the_number_of_vowels_more_than_30percent(){
+    void should_return_insert_mommy_for_each_continuous_set_of_vowels_when_insert_given_the_number_of_vowels_more_than_30percent(){
         //Given
         String testStr = "oooo";
         //When
         String result = new Mommifier().insertMommy(testStr);
         //Then
         String expect = "omommyomommyomommyo";
+        assertEquals(expect, result);
+    }
+
+    @Test
+    void should_return_not_insert_mommy_for_each_continuous_set_of_vowels_when_insert_given_the_number_of_vowels_no_more_than_30percent(){
+        //Given
+        String testStr = "lill";
+        //When
+        String result = new Mommifier().insertMommy(testStr);
+        //Then
+        String expect = "lill";
+        assertEquals(expect, result);
+    }
+
+    @Test
+    void should_return_not_insert_mommy_for_no_continuous_set_of_vowels_when_insert_given_the_number_of_vowels_more_than_30percent(){
+        //Given
+        String testStr = "lilil";
+        //When
+        String result = new Mommifier().insertMommy(testStr);
+        //Then
+        String expect = "lilil";
         assertEquals(expect, result);
     }
 }
